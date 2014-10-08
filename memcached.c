aa9916a07aafc643fcba6b80584abec6a484d2b1
june <abgood@sina.com>
2014-10-08 18:19:40 +0800

    hello memcached

---
+++ memcached.c
@@ -0,0 +1,6 @@
+#include "memcached.h"
+
+int main (int argc, char **argv) {
+    printf("memcached done\n");
+    return 0;
+}
---
+++ memcached.h
@@ -0,0 +1,10 @@
+#ifndef MEMCACHED_H
+#define MEMCACHED_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+#endif
